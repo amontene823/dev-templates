2c63a2b53ee53be4396a6df61421a27118689d9b
Angelo Montenegro <amontene823@gmail.com>
2026-04-06 06:38:35 +0000

    updated flake to handle numpy, wayland

--- flake.nix
+++ flake.nix
@@ -1,15 +1,86 @@
 {
-  description = "Project templates (devShells)";
+  description = "Develop Python on Nix with uv";
 
-  outputs = { self, ... }: {
-    templates.uv-qt = {
-      path = ./templates/uv-qt;
-      description = "uv + PySide6 (Qt) devShell template";
-    };
-
-    # Add more as you make them:
-    # templates.rust = { path = ./templates/rust; description = "..."; };
-
-    templates.default = self.templates.uv-qt;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
+
+  outputs = { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          qtRuntimeLibraries = with pkgs; [
+            libxkbcommon
+            xorg.libxcb
+            xorg.xcbutil
+            xorg.xcbutilcursor
+            xorg.xcbutilimage
+            xorg.xcbutilkeysyms
+            xorg.xcbutilrenderutil
+            xorg.xcbutilwm
+            xorg.libX11
+            xorg.libSM
+            xorg.libICE
+            fontconfig
+            freetype
+            dbus
+            glib                # <- provides libglib-2.0.so.0
+            wayland
+          ];
+
+          wheelRuntimeLibraries = with pkgs; [
+            zlib
+            zstd
+            stdenv.cc.cc.lib
+          ];
+
+          glRuntimeLibraries = with pkgs; [
+            mesa
+            libglvnd
+          ];
+
+          runtimeLibraries =
+            qtRuntimeLibraries
+            ++ wheelRuntimeLibraries
+            ++ glRuntimeLibraries;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python311
+              pkgs.uv
+            ] ++ runtimeLibraries;
+
+            env = (lib.optionalAttrs pkgs.stdenv.isLinux {
+              LD_LIBRARY_PATH = lib.makeLibraryPath runtimeLibraries;
+            }) // {
+              UV_PYTHON = "${pkgs.python311}/bin/python3.11";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+
+            shellHook = ''
+              unset PYTHONPATH
+
+              if [ -d .venv ]; then
+                if ! .venv/bin/python -c 'import sys' >/dev/null 2>&1; then
+                  rm -rf .venv
+                elif [ "$(readlink -f .venv/bin/python)" != "$(readlink -f "$UV_PYTHON")" ]; then
+                  rm -rf .venv
+                fi
+              fi
+
+              [ -d .venv ] || uv venv --python "$UV_PYTHON"
+              uv sync
+              . .venv/bin/activate
+            '';
+          };
+        }
+      );
+    };
 }
